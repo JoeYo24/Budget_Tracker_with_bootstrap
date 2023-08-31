7bd6d3b3ac3deeb7788138f26d1c7ee75a142914
JoeYo24 <jjsteffen@hotmail.com>
2023-08-31 16:02:17 -0400

    First Commit

---
+++ app/javascript/src/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1 className='text-success'>Hello World!</h1>
+    </div>
+  );
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.createElement('div');
+  document.body.appendChild(rootElement);
+
+  const root = createRoot(rootElement);
+  root.render(<Home />);
+});

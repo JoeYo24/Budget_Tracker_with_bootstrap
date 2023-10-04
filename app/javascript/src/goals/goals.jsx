25cf2c9eaa447813a654beb701336019bb58575c
JoeYo24 <jjsteffen@hotmail.com>
2023-10-04 18:18:46 -0400

    i've changed the way target dates on goals work

--- app/javascript/src/goals/goals.jsx
+++ app/javascript/src/goals/goals.jsx
@@ -88,6 +88,7 @@ const Goals = () => {
             <div key={index} className='goalItem'>
               <p>{goal.description}</p>
               <p>{goal.amount}</p>
+              <p>{goal.target_date}</p>
               <div className='progressBar'>
                 <div className='progress' style={{ width: `${goal.progress}%` }}>
                   {goal.progress}%
